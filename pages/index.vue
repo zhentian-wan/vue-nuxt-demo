9e56fbea4a48b3a2c5a2ede88db0c3b8a217ab2e
Zhentian Wan <answer881215@gmail.com>
2016-12-05 18:56:47 +0200

    first push

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <img src="~static/logo.png" alt="Nuxt.js Logo" />
+    <h1 class="title">
+      {{message}}
+    </h1>
+    <input v-model="message">
+    <router-link class="button" to="/about">
+      About page
+    </router-link>
+  </section>
+</template>
+
+<style scoped>
+.title
+{
+  margin: 50px 0;
+}
+</style>
+
+<script>
+  export default {
+    data(){
+      return {
+        message: "Hello World!"
+      }
+    }
+  }
+</script>
